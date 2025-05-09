65cd0691da9636b26b37fbd03091113c632af2a7
AhmedNuwar <nuwarahmed762@gmail.com>
2025-05-09 17:18:44 +0300

    new home page in progress

--- mainScript.js
+++ mainScript.js
@@ -21,32 +21,44 @@ document.addEventListener('DOMContentLoaded', function() {
     let walletNo = document.getElementById('wallet-number')|| "";
     let nid = document.getElementById('national-id')|| "";
 
-    userImg.src = `images/${currentUser.avatar}`;
-    userName.innerHTML = currentUser.name;
-    userEmail.innerHTML = currentUser.email;
-    if (currentUser.userType === 'Chief') {
-        userPhone.innerHTML = currentUser.phone;
-        userAddress.innerHTML = currentUser.address;
-        kitchName.innerHTML = currentUser.kitchenName;
-        bankNo.innerHTML = currentUser.bankAccount;
-        walletNo.innerHTML = currentUser.walletNo;
-        nid.innerHTML = currentUser.nid;
-    }
-    
-    // show/hide content based on user type
-    if (currentUser.userType === 'Chief') 
-    {
-        let chiefContent = document.querySelectorAll('.chief-content');
-        chiefContent.forEach(element => 
+    if (currentUser){
+        userImg.src = `images/${currentUser.avatar}`;
+        userName.innerHTML = currentUser.name;
+        userEmail.innerHTML = currentUser.email;
+
+        if (currentUser.userType === 'Chief') {
+            userPhone.innerHTML = currentUser.phone;
+            userAddress.innerHTML = currentUser.address;
+            kitchName.innerHTML = currentUser.kitchenName;
+            bankNo.innerHTML = currentUser.bankAccount;
+            walletNo.innerHTML = currentUser.walletNo;
+            nid.innerHTML = currentUser.nid;
+        }
+        
+        // show/hide content based on user type
+        if (currentUser.userType === 'Chief') 
         {
-            element.classList.toggle('show');
-        });
-        let userContent = document.querySelectorAll('.user-content');
-        userContent.forEach(element => {
-            element.classList.toggle('hide')
-        });
+            let chiefContent = document.querySelectorAll('.chief-content');
+            chiefContent.forEach(element => 
+            {
+                element.classList.toggle('show');
+            });
+            let userContent = document.querySelectorAll('.user-content');
+            userContent.forEach(element => {
+                element.classList.toggle('hide')
+            });
+        }
     }
         
     
+        
+    
 });
-
+function Run(id, url){
+    element = document.getElementById(id);
+    if(element) element.classList.add('active');
+    console.log(element,id, element.classList)
+    setTimeout(() => {
+        goTo(url);
+      }, 100);
+}
